0d6bb303c1db424365c518f9e20aaf8c54d6c5a1
NewMeeh <thomas.saillard@protonmail.com>
2022-06-22 21:31:12 +0200

    Co-authored-by: Hari <hari-jeya@users.noreply.github.com>

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -1,7 +1,7 @@
 import { getBio } from '../../api';
+import *  as fs from 'fs/promises';
 
 export const about = async (args: string[]): Promise<string> => {
-  const bio = await getBio();
-
+  const bio = fs.readFile('public/pages/about.md', 'utf8');
   return bio;
 };
