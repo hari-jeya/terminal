cef8a14c64fab24ad62d79581d652f3464328690
NewMeeh <thomas.saillard@protonmail.com>
2022-06-23 10:15:58 +0200

    Change Bio URL

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -1,7 +1,7 @@
 import { getBio } from '../../api';
-import *  as fs from 'fs/promises';
 
 export const about = async (args: string[]): Promise<string> => {
-  const bio = fs.readFile('public/pages/about.md', 'utf8');
+  const bio = await getBio();
+
   return bio;
 };
